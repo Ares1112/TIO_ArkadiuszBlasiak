830caf9ff8b3918d977a761b2093307c2a2a14db
Arkadiusz Blasiak <ares1112@gmail.com>
2016-04-24 03:48:02 -0700

    lab7

---
+++ lab7/EntityFrameworkTask/WebApplication1/Context/StoreContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Context
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(): base("StoreContext")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("public");
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ lab7/EntityFrameworkTask/WebApplication1/Initializers/StoreInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Initializers
+{
+    public class StoreInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<StoreContext>
+    {
+        protected override void Seed(StoreContext context)
+        {
+            List<Book> defaultBooks = new List<Book>();
+            defaultBooks.Add(new Book() { BookTitle = "BOOK1", ISBN = "123" });
+            defaultBooks.Add(new Book() { BookTitle = "BOOK2", ISBN = "1234" });
+            defaultBooks.Add(new Book() { BookTitle = "BOOK3", ISBN = "12345" });
+            foreach (Book b in defaultBooks)
+                context.Books.Add(b);
+            context.SaveChanges();
+
+            List<Author> defaultAuthors = new List<Author>();
+            defaultAuthors.Add(new Author { AuthorName = "NAME1", AuthorSurname = "SURNAME1" });
+            defaultAuthors.Add(new Author { AuthorName = "NAME2", AuthorSurname = "SURNAME2" });
+            defaultAuthors.Add(new Author { AuthorName = "NAME3", AuthorSurname = "SURNAME3" });
+            foreach (Author a in defaultAuthors)
+                context.Authors.Add(a);
+            context.SaveChanges();
+
+        }
+    }
+}
